a7f9a2040f3e3835efbd571f37ba587ca13cc1b0
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-05-07 20:47:11 +0200

    Add resolution information

--- stack_to_mmb_format.py
+++ stack_to_mmb_format.py
@@ -8,13 +8,13 @@ from mmb_utils import export_image_stack, initialize_image_dict, initialize_book
 ROOT = './data'
 
 
-def stack_to_mmb(input_folder, stack_name, target, max_jobs):
+def stack_to_mmb(input_folder, dataset_name, resolution, target, max_jobs):
     assert os.path.exists(input_folder), input_folder
 
-    tmp_folder = './tmp_%s' % stack_name
+    tmp_folder = './tmp_%s' % dataset_name
 
     # create output folder structure
-    output_folder = os.path.join(ROOT, stack_name)
+    output_folder = os.path.join(ROOT, dataset_name)
     make_folder_structure(output_folder)
 
     # convert tif stack to bdv.n5 format
@@ -22,8 +22,6 @@ def stack_to_mmb(input_folder, stack_name, target, max_jobs):
     # @tischi and me need to update this eventually
     out_path = os.path.join(output_folder, 'images', 'local', 'sbem-6dpf-1-whole-raw.n5')
 
-    # TODO need resolution information
-    resolution = [1, 1, 1]
     export_image_stack(input_folder, out_path, tmp_folder, resolution,
                        target=target, max_jobs=max_jobs)
 
@@ -33,7 +31,18 @@ def stack_to_mmb(input_folder, stack_name, target, max_jobs):
     initialize_bookmarks(output_folder)
 
     # register this stack in versions.json
-    add_version(stack_name, ROOT)
+    add_version(dataset_name, ROOT)
+
+
+# infer resolution if it was set to None
+def get_resolution(resolution, name):
+    if resolution is None:
+        # TODO we need the exact 5nm identifier in the filename
+        if '' in name:
+            resolution = [0.005, 0.005, 0.005]
+        else:
+            resolution = [0.008, 0.008, 0.008]
+    return resolution
 
 
 # This is the exaple path from Julian (without white-spaces now)
@@ -42,8 +51,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_folder', type=str)
     parser.add_argument('name', type=str)
+    parser.add_argument('--resolution', type=int, nargs=3, default=None)
     parser.add_argument('--target', type=str, default='local')
     parser.add_argument('--max_jobs', type=int, default=16)
 
     args = parser.parse_args()
-    stack_to_mmb(args.input_folder, args.name, args.target, args.max_jobs)
+    resolution = get_resolution(args.resolution, args.name)
+    stack_to_mmb(args.input_folder, args.name, resolution, args.target, args.max_jobs)

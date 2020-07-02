1935c63fcfd3526b3b6047cf21b3474dfeb30b25
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2020-07-02 11:46:20 +0200

    Fix handling the resolution parameter from the command line

--- add_seg_to_dataset.py
+++ add_seg_to_dataset.py
@@ -1,10 +1,12 @@
 import argparse
+import json
 import os
 
 from mobie import add_segmentation
 from stack_to_mmb_format import add_xml_for_s3
 
 ROOT = './data'
+DEFAULT_RESOLUTION = [0.008, 0.008, 0.008]
 DEFAULT_CHUNKS = (64, 64, 64)
 
 
@@ -34,11 +36,18 @@ if __name__ == '__main__':
     parser.add_argument('input_key', type=str)
     parser.add_argument('dataset_name', type=str)
     parser.add_argument('segmentation_name', type=str)
-    parser.add_argument('--resolution', type=float, nargs=3, default=None)
+    parser.add_argument('--resolution', type=str, default=None,
+                        help='Resolution in micrometer. Needs to be json encoded string; defaults to 0.008 um.')
     parser.add_argument('--chunks', type=int, nargs=3, default=DEFAULT_CHUNKS)
     parser.add_argument('--target', type=str, default='local')
     parser.add_argument('--max_jobs', type=int, default=16)
 
     args = parser.parse_args()
+    resolution = args.resolution
+    if resolution is None:
+        resolution = DEFAULT_RESOLUTION
+    else:
+        resolution = json.loads(resolution)
+
     add_seg_to_dataset(args.input_path, args.input_key, args.dataset_name, args.segmentation_name,
-                       args.resolution, args.chunks, args.target, args.max_jobs)
+                       resolution, args.chunks, args.target, args.max_jobs)
